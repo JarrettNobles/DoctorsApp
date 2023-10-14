f29782b0fc18de228f57efc7c2747825b1166347
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-14 12:13:50 -0400

    Added styling and variables for the login screen

--- DoctorsApp/ContentView.swift
+++ DoctorsApp/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    //username variable
+    @State private var username = ""
+    //password variable
+    @State private var password = ""
+    //wrong
+    @State private var wrongUsername = 0
+    //wrong password
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("First Commit")
+        NavigationView{
+            ZStack{
+                Color.blue.ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+            }
+            
         }
-        .padding()
+        .navigationBarHidden(true)
     }
+   
+   
+    
 }
-
+//dont delete right now
 #Preview {
     ContentView()
 }
