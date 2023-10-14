36678f9afd6e166548f00c7f88c1657593467ad6
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-14 11:50:24 -0400

    Initial Commit

---
+++ DoctorsApp/DoctorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsAppApp.swift
+//  DoctorsApp
+//
+//  Created by jarrett nobles on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
