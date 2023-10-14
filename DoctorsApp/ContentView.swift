cecbbbe7fd945cac0b8a418b79224f1dd498f532
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-14 12:47:06 -0400

    added comments and better optimized the code

--- DoctorsApp/ContentView.swift
+++ DoctorsApp/ContentView.swift
@@ -1,27 +1,21 @@
-//
-//  ContentView.swift
-//  DoctorsApp
-//
-//  Created by jarrett nobles on 10/14/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    //username variable
+    // User input variables
     @State private var username = ""
-    //password variable
     @State private var password = ""
-    //wrong
-    @State private var wrongUsername = 0
-    //wrong password
-    @State private var wrongPassword = 0
+
+    // Error flags for indicating wrong username or password
+    @State private var wrongUsername = false
+    @State private var wrongPassword = false
+
+    // Flag to control navigation to the logged-in view
     @State private var showingLoginScreen = false
-    
-    
+
     var body: some View {
-        NavigationView{
-            ZStack{
+        NavigationView {
+            ZStack {
+                // Background circles
                 Color.blue.ignoresSafeArea()
                 Circle()
                     .scale(1.7)
@@ -29,82 +23,71 @@ struct ContentView: View {
                 Circle()
                     .scale(1.35)
                     .foregroundColor(.white)
-                VStack{
-                    //login text
+
+                VStack {
+                    // Login title
                     Text("Login")
                         .font(.largeTitle)
                         .bold()
                         .padding()
-                    //TextField for username
-                    TextField("Username",text:$username)
+
+                    // Username input field
+                    TextField("Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.black.opacity(0.05))
-                        .cornerRadius(10)
-                        .border(.red,width:CGFloat(wrongUsername))
-                    //Password securefield
-                    SecureField("Password",text:$password)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(wrongUsername ? Color.red : Color.clear, lineWidth: 2)
+                        )
+
+                    // Password input field
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.black.opacity(0.05))
-                        .cornerRadius(10)
-                        .border(.red,width:CGFloat(wrongPassword))
-                    //login button
-                    Button("Login"){
-                        //logic to authenticate
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(wrongPassword ? Color.red : Color.clear, lineWidth: 2)
+                        )
+
+                    // Login button
+                    Button("Login") {
                         authenticateUser(username: username, password: password)
-                        
                     }
                     .foregroundColor(.white)
-                    .frame(width:300, height:50)
-                    .background(Color.blue).cornerRadius(10)
-                    
-                    //navigation link
-                    //change to home page eventually
-                   // NavigationLink(destination: Text("this is the logged in view @\(username)"),isActive:$showingLoginScreen){
-                                            EmptyView()
-                        
-                    //}
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+
+                    // Navigation link to the logged-in view
                     NavigationLink(
-                                        destination: Text("This is the logged-in view @\(username)"),
-                                        isActive: $showingLoginScreen,
-                                        label: { EmptyView() }
-                                    )
-                    
+                        destination: Text("This is the logged-in view @\(username)"),
+                        isActive: $showingLoginScreen,
+                        label: { EmptyView() }
+                    )
                 }
-                
             }
             .navigationBarHidden(true)
         }
-        
-        
-        
-    
-}
-    //fill in with database info
-    func authenticateUser(username: String, password: String){
-        if username.lowercased() == "Test"{
-            wrongUsername = 0
-            if password.lowercased() == "test"{
-                wrongPassword = 0
-                showingLoginScreen = true
-            }
-            
-            else{
-                wrongPassword = 2
-            }
-            
-        }
-        
-        else{
-            wrongUsername = 2
-        }
     }
 
-
+    // Function to authenticate the user
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "test" {
+            wrongUsername = false
+            if password.lowercased() == "test" {
+                wrongPassword = false
+                showingLoginScreen = true
+            } else {
+                wrongPassword = true
+            }
+        } else {
+            wrongUsername = true
+        }
+    }
 }
 
-//dont delete right now
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
