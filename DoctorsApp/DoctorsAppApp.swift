44e9416e8a7be38655455931d386fa1a06148636
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-14 14:00:51 -0400

    Updated the main state of the app

--- DoctorsApp/DoctorsAppApp.swift
+++ DoctorsApp/DoctorsAppApp.swift
@@ -4,14 +4,17 @@
 //
 //  Created by jarrett nobles on 10/14/23.
 //
-
 import SwiftUI
 
 @main
 struct DoctorsAppApp: App {
+    @StateObject var userState = UserState()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(userState)
         }
     }
 }
+
