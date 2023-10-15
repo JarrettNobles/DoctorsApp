70c78e20cb8d8404eb954e6ff289112bd2d884af
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-14 20:17:23 -0400

    updated profiles page

--- DoctorsApp/UserProfileView.swift
+++ DoctorsApp/UserProfileView.swift
@@ -11,23 +11,46 @@ struct UserProfileView: View {
     @EnvironmentObject var userState: UserState
 
     var body: some View {
-        VStack {
-            // Display the user's name
-            Text("Welcome, \(userState.username)!")
-                .font(.title)
+        ZStack(alignment: .top) {
+            // Blue box that fills the top quarter of the screen
+            Color.blue
+                .frame(height: UIScreen.main.bounds.height / 4)
+                .ignoresSafeArea()
+
+            VStack(alignment: .center, spacing: 0) {
+                // Display the profile picture (replace "placeholder" with the actual image)
+                Image("ProfilePictures/jarrett.jpg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80) // Adjust the size of the profile picture
+                    .clipShape(Circle())
+                    .padding()
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: 5)
+                    .offset(y: 60) // Move the profile picture downward
+
+                // Display the welcome message with the user's name
+                Text(userState.username)
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .padding(.top, 80) // Add space between the profile picture and the name
+
+                Spacer() // Spacer to push content below the user name to the bottom
+
+                // Use VStack with alignment .center to center the remaining content
+                VStack(alignment: .center) {
+                    // Display the list of medications
+                    MedicationListView(medications: userState.medications)
+
+                    // Display the list of medical conditions
+                    MedicalConditionListView(conditions: userState.medicalConditions)
+                }
                 .padding()
+                .frame(maxWidth: .infinity, alignment: .center) // Center content horizontally
 
-            // Display the list of medications
-            MedicationListView(medications: userState.medications)
-
-            // Display the list of medical conditions
-            MedicalConditionListView(conditions: userState.medicalConditions)
-
-            // Add more content for the user profile as needed
+                Spacer() // Spacer to push content above medications and conditions to the bottom
+            }
+            .navigationBarTitle("User Profile", displayMode: .inline)
         }
-        .padding()
-        .navigationBarTitle("User Profile")
     }
 }
-
-
