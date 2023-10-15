0a0de46133af93f45cc67b55c2a7aaff08747043
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-10-15 13:34:37 -0400

    Added comments

--- DoctorsApp/ContentView.swift
+++ DoctorsApp/ContentView.swift
@@ -5,30 +5,44 @@ import FirebaseFirestore
 import FirebaseAuth
 
 struct ContentView: View {
+    // State object to manage user's authentication state
     @StateObject var userState = UserState()
 
+    // Variables to store username and password input
     @State private var username = ""
     @State private var password = ""
+
+    // Flags to indicate if there are issues with the entered username and password
     @State private var wrongUsername = false
     @State private var wrongPassword = false
+
+    // Flag to control navigation to the user profile screen
     @State private var showingLoginScreen = false
 
     var body: some View {
         NavigationView {
+            // ZStack for layered UI elements
             ZStack {
+                // Background color filling the safe area
                 Color.blue.ignoresSafeArea()
+
+                // Two concentric circles for decorative effect
                 Circle()
                     .scale(1.7)
                     .foregroundColor(.white.opacity(0.15))
                 Circle()
                     .scale(1.35)
                     .foregroundColor(.white)
+
+                // VStack to arrange UI elements vertically
                 VStack {
+                    // Title of the screen
                     Text("Login")
                         .font(.largeTitle)
                         .bold()
                         .padding()
 
+                    // Text field for entering the username
                     TextField("Username", text: $username)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
@@ -37,6 +51,7 @@ struct ContentView: View {
                                 .stroke(wrongUsername ? Color.red : Color.clear, lineWidth: 2)
                         )
 
+                    // Secure text field for entering the password
                     SecureField("Password", text: $password)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
@@ -45,6 +60,7 @@ struct ContentView: View {
                                 .stroke(wrongPassword ? Color.red : Color.clear, lineWidth: 2)
                         )
 
+                    // Button to trigger the authentication process
                     Button("Login") {
                         authenticateUser(username: username, password: password)
                     }
@@ -53,13 +69,14 @@ struct ContentView: View {
                     .background(Color.blue)
                     .cornerRadius(10)
 
+                    // Navigation link to the user profile screen
                     NavigationLink(
                         destination: UserProfileView().environmentObject(userState),
                         isActive: $showingLoginScreen,
                         label: { EmptyView() }
                     )
 
-                    // New NavigationLink for creating an account
+                    // Navigation link to the account creation screen
                     NavigationLink(destination: CreateAccountView()) {
                         Text("Create an Account")
                             .foregroundColor(.blue)
@@ -68,10 +85,11 @@ struct ContentView: View {
                     .padding(.top, 20) // Add padding to separate the links
                 }
             }
-            .navigationBarHidden(true)
+            .navigationBarHidden(true) // Hide the navigation bar
         }
     }
 
+    // Function to authenticate the user based on entered credentials
     func authenticateUser(username: String, password: String) {
         if username.lowercased() == "test" {
             wrongUsername = false
--- DoctorsApp/CreateAccountView.swift
+++ DoctorsApp/CreateAccountView.swift
@@ -10,21 +10,27 @@ import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
-
 struct CreateAccountView: View {
+    // State variables to store new account information
     @State private var newUsername = ""
     @State private var newPassword = ""
     @State private var confirmPassword = ""
 
+    // Flags to indicate if there are issues with the entered new username and password
     @State private var wrongNewUsername = false
     @State private var wrongNewPassword = false
 
+    // Flag to control navigation to the content view
     @State private var navigateToContentView = false
 
     var body: some View {
         NavigationView {
+            // ZStack for layered UI elements
             ZStack {
+                // Background color filling the safe area
                 Color.blue.ignoresSafeArea()
+
+                // Two concentric circles for decorative effect
                 Circle()
                     .scale(1.7)
                     .foregroundColor(.white.opacity(0.15))
@@ -32,14 +38,18 @@ struct CreateAccountView: View {
                     .scale(1.35)
                     .foregroundColor(.white)
 
+                // VStack to arrange UI elements vertically
                 VStack {
+                    // Spacer to push content to the top
                     Spacer()
 
+                    // Title of the screen
                     Text("Create an Account")
                         .font(.largeTitle)
                         .bold()
                         .padding()
 
+                    // Text field for entering the new username
                     TextField("Username", text: $newUsername)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
@@ -48,6 +58,7 @@ struct CreateAccountView: View {
                                 .stroke(wrongNewUsername ? Color.red : Color.clear, lineWidth: 2)
                         )
 
+                    // Secure text field for entering the new password
                     SecureField("Password", text: $newPassword)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
@@ -56,10 +67,12 @@ struct CreateAccountView: View {
                                 .stroke(wrongNewPassword ? Color.red : Color.clear, lineWidth: 2)
                         )
 
+                    // Secure text field for confirming the new password
                     SecureField("Confirm Password", text: $confirmPassword)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
 
+                    // Button to trigger the creation of the new account
                     Button("Create Account") {
                         // Add logic to create the new account
                         createAccount(username: newUsername, password: newPassword)
@@ -71,6 +84,7 @@ struct CreateAccountView: View {
                     .background(Color.blue)
                     .cornerRadius(10)
 
+                    // Text to prompt users to log in if they already have an account
                     Text("Already have an account? Log in.")
                         .foregroundColor(.blue)
                         .onTapGesture {
@@ -78,17 +92,22 @@ struct CreateAccountView: View {
                             navigateToContentView = true
                         }
 
+                    // Spacer to push content to the bottom
                     Spacer()
                 }
             }
+            // Hide the navigation bar
             .navigationBarHidden(true)
+            // Full-screen cover to navigate to ContentView
             .fullScreenCover(isPresented: $navigateToContentView) {
                 ContentView()
             }
+            // Remove any padding
             .padding(0)
         }
     }
 
+    // Function to create a new account based on entered credentials
     func createAccount(username: String, password: String) {
         // Add logic to create the new account
         // You can use Firebase Authentication methods here
