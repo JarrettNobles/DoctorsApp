415fbc64690e9ff8b25e3ba5e22e3ea3cd954975
Jarrett Nobles <noblesjarrett035@gmail.com>
2023-11-06 19:35:32 -0500

    updating more comments

--- DoctorsApp/MedicalConditionListView.swift
+++ DoctorsApp/MedicalConditionListView.swift
@@ -10,23 +10,26 @@ import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
-
-
+// This struct defines a SwiftUI view for displaying a list of medical conditions.
 struct MedicalConditionListView: View {
+    // An array of medical conditions that will be displayed in the view.
     let conditions: [String]
 
     var body: some View {
+        // Create a vertical stack to arrange the view's content from top to bottom.
         VStack(alignment: .leading) {
+            // Display a headline text for the section.
             Text("Medical Conditions:")
-                .font(.headline)
-                .padding(.bottom, 5)
+                .font(.headline) // Set the text style to headline.
+                .padding(.bottom, 5) // Add a small space below the headline.
 
-            // Display the list of medical conditions
+            // Use a ForEach loop to iterate through the list of medical conditions.
             ForEach(conditions, id: \.self) { condition in
+                // Display each medical condition with a bullet point.
                 Text("- \(condition)")
             }
         }
-        .padding(.bottom, 10)
+        .padding(.bottom, 10) // Add some space at the bottom of the stack.
     }
 }
 
